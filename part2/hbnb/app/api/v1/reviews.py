4cdf8508eb2ada5494891a86290579b42ddbc559
Fantasar <lapique.philippe@gmail.com>
2025-10-14 15:09:20 +0200

    Modification du corp du fichier avec l'implantation des fonctions pour --> Ajouter un avis, suprimmer un avis, mettre à jour un avis ou récupérer des avis avec un ID

--- part2/hbnb/app/api/v1/reviews.py
+++ part2/hbnb/app/api/v1/reviews.py
@@ -1,8 +1,9 @@
 from flask_restx import Namespace, Resource, fields
-from app.services import facade
+from app.services.facade import HBnBFacade
 
 # ------------------------------------------------------------
 
+facade = HBnBFacade()
 api = Namespace('reviews', description='Review operations')
 
 # Define the review model for input validation and documentation
@@ -22,16 +23,32 @@ class ReviewList(Resource):
     @api.response(400, 'Invalid input data')
     def post(self):
         """Register a new review"""
-        # Placeholder for the logic to register a new review
-        pass
+        review_data = api.payload
+        new_review = facade.create_review(review_data)
+        return {
+            'id': new_review.id,
+            'text': new_review.text,
+            'rating': new_review.rating,
+            'user_id': new_review.user_id,
+            'place_id': new_review.place_id,
+            'created_at': getattr(new_review, 'created_at', None)
+        }, 201
 
 # ------------------------------------------------------------
 
     @api.response(200, 'List of reviews retrieved successfully')
     def get(self):
         """Retrieve a list of all reviews"""
-        # Placeholder for logic to return a list of all reviews
-        pass
+        reviews = facade.get_all_reviews()
+        return[
+            {
+                'id': r.id,
+                'text': r.text,
+                'rating': r.rating,
+                'user_id': r.user_id,
+                'place_id': r.place_id
+            }for r in reviews
+        ], 200
 
 # ------------------------------------------------------------
 
@@ -42,7 +59,16 @@ class ReviewResource(Resource):
     def get(self, review_id):
         """Get review details by ID"""
         # Placeholder for the logic to retrieve a review by ID
-        pass
+        review = facade.get_review(review_id)
+        if not review:
+            return {'error': 'Review not found'}, 404
+        return {
+            'id': review.id,
+            'text': getattr(review, 'text', None),
+            'rating': getattr(review, 'rating', None),
+            'user_id': review.user_id,
+            'place_id': review.place_id 
+        }, 200
 
 # ------------------------------------------------------------
 
@@ -53,7 +79,19 @@ class ReviewResource(Resource):
     def put(self, review_id):
         """Update a review's information"""
         # Placeholder for the logic to update a review by ID
-        pass
+        data = api.payload
+
+        updated_review = facade.update_review(review_id, data)
+        if not updated_review:
+            return {'error': 'Place not found'}, 404
+
+        return {
+            'id': update_review.id,
+            'text': update_review.text,
+            'rating': update_review.rating,
+            'user_id': update_review.user_id,
+            'place_id': update_review.place_id
+        }, 200
 
 # ------------------------------------------------------------
 
@@ -62,7 +100,10 @@ class ReviewResource(Resource):
     def delete(self, review_id):
         """Delete a review"""
         # Placeholder for the logic to delete a review
-        pass
+        deleted = facade.delete_review(review_id)
+        if not deleted:
+            return {'error': 'Review not found'}, 404
+        return {'message': 'Review deleted successfully'}, 200
 
 # ------------------------------------------------------------
 
@@ -73,6 +114,14 @@ class PlaceReviewList(Resource):
     def get(self, place_id):
         """Get all reviews for a specific place"""
         # Placeholder for logic to return a list of reviews for a place
-        pass
+        reviews = facade.get_reviews_by_place(place_id)
+        return [
+            {
+                'id': r.id,
+                'text': r.text,
+                'rating': r.rating,
+                'user_id': r.user_id
+            }for r in reviews
+        ], 200
 
 # ------------------------------------------------------------
